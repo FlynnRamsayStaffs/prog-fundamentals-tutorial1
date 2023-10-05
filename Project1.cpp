d98e569e844a1e4335c63692ec5df66e34d223a5
FlynnRamsayStaffs <r010722n@student.staffs.ac.uk>
2023-10-05 14:09:36 +0100

    added code from challenges

---
+++ Project1.cpp
@@ -0,0 +1,30 @@
+// Project1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int jer = 3;
+    cout << "Hello World!\n";
+    cout << "Flynn Ramsay\n";
+    cout << "r010277n\n";
+    cout << "   *\n";
+    cout << "   * *\n";
+    cout << "   *  *\n";
+    cout << "   *   *\n";
+    cout << "   ******\n";
+    cout << "     |    \n";
+    cout << " ***********\n";
+    cout << " *         *\n";
+    cout << " ***********\n";
+
+    cout << "Hello...  \t\t\t there\n";
+    cout << "2 + 2" << " = " << "4\n";
+
+
+    cout << jer << "\n";  
+    cout << "test";
+            
+}
+
